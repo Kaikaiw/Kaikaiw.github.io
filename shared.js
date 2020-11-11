5cf727fcf6996bffd56e67d89f87d56ab1c0dd58
kai- <kaikaiw321@gmail.com>
2020-11-10 23:57:35 -0800

    防作弊更新
    ^
    负数

--- shared.js
+++ shared.js
@@ -309,7 +309,7 @@ class PlayerState extends EntityState {
     var key = input.key;
     var delta = input.delta;
 
-    if (delta > 20) { // Anti-cheat
+    if (delta > 20 || delta <= 0) { // Anti-cheat
       return;
     }
 
