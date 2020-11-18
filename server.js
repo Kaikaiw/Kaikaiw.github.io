8a7f3720f8bcfe0b57e54cd1b21fbb8caf13e82b
kai- <kaikaiw321@gmail.com>
2020-11-17 20:30:46 -0800

    秒平均包数
    ^

--- server.js
+++ server.js
@@ -12,6 +12,30 @@ function initGame() {
   }, 1000.0 / E.SERVER_FRAME);
 }
 
+var tsMap = {};
+var ctrMap = {};
+function calcAvgCtr(id) {
+  var nowTs = +new Date();
+  if (!(id in tsMap)) {
+    tsMap[id] = nowTs;
+  }
+
+  var previousSecond = Math.floor(tsMap[id] / 1000);
+  var nowSecond = Math.floor(nowTs / 1000);
+
+  if (nowSecond == previousSecond) {
+    if (!(id in ctrMap)) {
+      ctrMap[id] = 0;
+    }
+    ctrMap[id]++;
+  } else {
+    console.log(id, previousSecond, ctrMap[id]);
+    ctrMap[id] = 1;
+  }
+
+  tsMap[id] = nowTs;
+}
+
 function init() {
   initGame();
   // 网络
@@ -20,6 +44,9 @@ function init() {
     var id = socket.request.connection.remoteAddress;
     E.spawnPlayer(id, socket);
     socket.on('opcode', (msg) => {
+      if (msg.opcode == E.types.opcode.move) {
+        calcAvgCtr(id);
+      }
       E.recvMessage(id, msg);
     });
     socket.on('disconnect', (reason) => {
